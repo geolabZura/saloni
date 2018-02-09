9163c42ee98250ace28e08b25419fa3656a461ec
geolabZura <zurab.tsintsadze@geolab.edu.ge>
2018-02-09 05:10:24 +0400

    add slider

--- app/Http/Controllers/Site/MainPageController.php
+++ app/Http/Controllers/Site/MainPageController.php
@@ -4,26 +4,43 @@ namespace App\Http\Controllers;
 
 use App\AboutUs;
 use App\BackgroundImage;
+use App\Service;
 use Illuminate\Http\Request;
 
 class MainPageController extends Controller
 {
     protected $background_images;
     protected $about_us;
+    protected $service;
 
     public function __construct(){
         $this->background_images = new BackgroundImage();
         $this->about_us = new AboutUs();
+        $this->service = new Service();
     }
-
     public function index(){
         $background_images = $this->background_images->all();
+        $data['services'] = $this->service->all();
+        $service_count = 1;
+        $service_index_split_count = 0;
 
-        foreach ($background_images as $images){
+        foreach ($background_images as $images) {
             $data[$images->position_name] = $images->image;
         }
+//
+//        foreach ($services as $service){
+//            if($service_count>3){
+//                $service_index_split_count++;
+//                $service_count=1;
+//                $data['services'][$service_index_split_count][] = $service;
+//            }else{
+//                $service_count++;
+//                $data['services'][$service_index_split_count][] = $service;
+//            }
+//        }
         $data['about_us'] = $this->about_us->first();
 
         return view('index', $data);
     }
+
 }
--- resources/views/site/partials/slider.blade.php
+++ resources/views/site/partials/slider.blade.php
@@ -2,125 +2,81 @@
     <div style="position: relative !important;">
 
         <div class="SlideSectionContainer owl-carousel">
-            <div class="PageHeight SlideSectionContainerSlide">
-                <section class="SlideTride">
-                    <div class="TrideText">
-                        <h2 class="text-center TrideTitle"> СПА Зона</h2>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList ">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <button class="text-uppercase">ВСЕ УСЛУГИ</button>
-                    </div><!--.TrideText -->
-                    <div class="SliderImgDiv" style="background-image:url({{asset('/site/image/uslugi1.jpg')}})"></div>
-                </section>
-                <section class="SlideTride">
-                    <div class="SliderImgDiv" style="background-image:url({{asset('/site/image/uslugi2.jpg')}}"></div>
-                    <div class="TrideText">
-                        <h2 class="text-center TrideTitle"> Косметология тело</h2>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <button class="text-uppercase">ВСЕ УСЛУГИ</button>
-                    </div><!--.TrideText -->
-                </section>
-                <section class="SlideTride">
-                    <div class="TrideText">
-                        <h2 class="text-center TrideTitle">Косметология лица </h2>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            Коррекция фгуры
-                            <span class="Price">9999</span>
-                        </p>
-                        <button class="text-uppercase">ВСЕ УСЛУГИ</button>
-                    </div><!--.TrideText -->
-                    <div class="SliderImgDiv" style="background-image:url({{asset('/site/image/uslugi3.jpg')}})"></div>
-                </section>
-            </div>
-            <div class="PageHeight SlideSectionContainerSlide">
-                <section class="SlideTride">
-                    <div class="SliderImgDiv" style="background-image:url({{asset('site/image/uslugi1.jpg')}}"></div>
-                    <div class="TrideText">
-                        <h2 class="text-center TrideTitle"> СПА Зона</h2>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <button class="text-uppercase">ВСЕ УСЛУГИ</button>
-                    </div><!--.TrideText -->
-                </section>
-                <section class="SlideTride">
-                    <div class="TrideText">
-                        <h2 class="text-center TrideTitle"> Косметология тело</h2>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <button class="text-uppercase">ВСЕ УСЛУГИ</button>
-                    </div><!--.TrideText -->
-                    <div class="SliderImgDiv" style="background-image:url({{asset('/site/image/uslugi2.jpg')}})"></div>
-                </section>
 
-                <section class="SlideTride">
-                    <div class="SliderImgDiv" style="background-image:url({{asset('/site/image/uslugi3.jpg')}})"></div>
-                    <div class="TrideText">
-                        <h2 class="text-center TrideTitle">Косметология лица </h2>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            banya maslova
-                            <span class="Price">9999</span>
-                        </p>
-                        <p class="TrideList">
-                            Коррекция фгуры
-                            <span class="Price">9999</span>
-                        </p>
-                        <button class="text-uppercase">ВСЕ УСЛУГИ</button>
-                    </div><!--.TrideText -->
-                </section>
-            </div>
+            @if(!empty($services))
+                @foreach($services as $service)
+
+                    @if($loop->iteration===1)
+                        <div class="PageHeight SlideSectionContainerSlide">
+                            <section class="SlideTride">
+                                <div class="TrideText">
+                                    <h2 class="text-center TrideTitle">{{$loop->iteration}} СПА Зона</h2>
+                                    @if(!empty($service->categories))
+                                        @foreach($service->categories as $category)
+                                            @if($loop->iteration>3)
+                                                @break
+                                            @else
+                                                <p class="TrideList">
+                                                    {{$category->name}}
+                                                    <span class="Price">{{$category->price}}</span>
+                                                </p>
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                    <button class="text-uppercase">ВСЕ УСЛУГИ</button>
+                                </div><!--.TrideText -->
+                                <div class="SliderImgDiv" style="background-image:url({{asset('/image/'.$service->image)}})"></div>
+                            </section>
+                        </div>
+
+                    @elseif($loop->iteration%3==0)
+                        <div class="PageHeight SlideSectionContainerSlide">
+                            <section class="SlideTride">
+                                <div class="TrideText">
+                                    <h2 class="text-center TrideTitle">{{$loop->iteration}} СПА Зона</h2>
+                                    @if(!empty($service->categories))
+                                        @foreach($service->categories as $category)
+                                            @if($loop->iteration>3)
+                                                @break
+                                            @else
+                                                <p class="TrideList">
+                                                    {{$category->name}}
+                                                    <span class="Price">{{$category->price}}</span>
+                                                </p>
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                    <button class="text-uppercase">ВСЕ УСЛУГИ</button>
+                                </div><!--.TrideText -->
+                                <div class="SliderImgDiv" style="background-image:url({{asset('/image/'.$service->image)}})"></div>
+                            </section>
+                        </div>
+                    @elseif($loop->iteration%2==0)
+                        <div class="PageHeight SlideSectionContainerSlide">
+                            <section class="SlideTride">
+                                <div class="SliderImgDiv" style="background-image:url({{asset('/image/'.$service->image)}})"></div>
+
+                                <div class="TrideText">
+                                    <h2 class="text-center TrideTitle">{{$loop->iteration}} СПА Зона</h2>
+                                    @if(!empty($service->categories))
+                                        @foreach($service->categories as $category)
+                                            @if($loop->iteration>3)
+                                                @break
+                                            @else
+                                                <p class="TrideList">
+                                                    {{$category->name}}
+                                                    <span class="Price">{{$category->price}}</span>
+                                                </p>
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                    <button class="text-uppercase">ВСЕ УСЛУГИ</button>
+                                </div><!--.TrideText -->
+                            </section>
+                        </div>
+                    @endif
+                @endforeach
+            @endif
         </div>
-    </div><!-- .row -->
-</div><!-- .Slide, .container-fluid -->
+    </div>
+</div>
