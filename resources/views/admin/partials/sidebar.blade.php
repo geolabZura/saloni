d5a7edd7c320c6ea1c2392f5bd4f0d2fe120f813
geolabZura <zurab.tsintsadze@geolab.edu.ge>
2018-03-20 18:14:32 +0400

    Asdasdasd

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -20,28 +20,28 @@
 
             <!--treeview navigation need class .treeview-->
 
-            <li class="{{Request::is('admin/icon') ? 'active ' : ''}}">
-                <a href="{{route('admin.social')}}">
-                    <i class="fa fa-dashboard"></i> <span>Social Site Links</span>
+            <li class="{{Request::is('admin/category') ? 'active ' : ''}}">
+                <a href="{{route('admin.category')}}">
+                    <i class="fa fa-bars" aria-hidden="true"></i><span>Service Category</span>
                 </a>
             </li>
 
-            <li class="{{Request::is('admin/category') ? 'active ' : ''}}">
-                <a href="{{route('admin.category')}}">
-                    <i class="fa fa-dashboard"></i> <span>Service Category</span>
+            <li class="{{Request::is('admin/service') ? 'active ' : ''}}">
+                <a href="{{route('admin.service')}}">
+                    <i class="fa fa-cog" aria-hidden="true"></i><span>Services</span>
                 </a>
             </li>
 
             <li class="{{Request::is('admin/images') ? 'active ' : ''}}">
                 <a href="{{route('admin.images')}}">
-                    <i class="fa fa-dashboard"></i> <span>Background Images</span>
+                    <i class="fa fa-picture-o" aria-hidden="true"></i><span>Background Images</span>
                 </a>
             </li>
 
 
             <li class="treeview {{(Request::is('admin/aboutus')  || Request::is('admin/aboutstaff')) ? 'active ' : ''}}">
                 <a href="">
-                    <i class="fa fa-table"></i> <span>Tables</span>
+                    <i class="fa fa-info-circle" aria-hidden="true"></i><span>About</span>
                     <span class="pull-right-container">
                             <i class="fa fa-angle-left pull-right"></i>
                         </span>
@@ -53,47 +53,46 @@
                 </ul>
             </li>
 
-
-            <li class="{{Request::is('admin/service') ? 'active ' : ''}}">
-                <a href="{{route('admin.service')}}">
-                    <i class="fa fa-dashboard"></i> <span>Services</span>
-                </a>
-            </li>
-
             <li class="{{Request::is('admin/staff') ? 'active ' : ''}}">
                 <a href="{{route('admin.staff')}}">
-                    <i class="fa fa-dashboard"></i> <span>Staff</span>
+                    <i class="fa fa-users" aria-hidden="true"></i><span>Staff</span>
                 </a>
             </li>
 
             <li class="{{Request::is('admin/brand') ? 'active ' : ''}}">
                 <a href="{{route('admin.brand')}}">
-                    <i class="fa fa-dashboard"></i> <span>Brand</span>
+                    <i class="fa fa-pagelines" aria-hidden="true"></i> <span>Brand</span>
                 </a>
             </li>
 
 
             <li class="{{Request::is('admin/offer') ? 'active ' : ''}}">
                 <a href="{{route('admin.offer')}}">
-                    <i class="fa fa-dashboard"></i> <span>Offer</span>
+                    <i class="fa fa-handshake-o" aria-hidden="true"></i><span>Offer</span>
                 </a>
             </li>
 
             <li class="{{Request::is('admin/gallery') ? 'active ' : ''}}">
                 <a href="{{route('admin.gallery')}}">
-                    <i class="fa fa-dashboard"></i> <span>Images Gallery</span>
+                    <i class="fa fa-file-image-o" aria-hidden="true"></i><span>Gallery</span>
                 </a>
             </li>
 
             <li class="{{Request::is('admin/contact') ? 'active ' : ''}}">
                 <a href="{{route('admin.contact')}}">
-                    <i class="fa fa-dashboard"></i> <span>Contacts</span>
+                    <i class="fa fa-phone" aria-hidden="true"></i><span>Contact</span>
                 </a>
             </li>
 
             <li class="{{Request::is('admin/map') ? 'active ' : ''}}">
                 <a href="{{route('admin.map')}}">
-                    <i class="fa fa-dashboard"></i> <span>Map</span>
+                    <i class="fa fa-location-arrow" aria-hidden="true"></i><span>Map</span>
+                </a>
+            </li>
+
+            <li class="{{Request::is('admin/icon') ? 'active ' : ''}}">
+                <a href="{{route('admin.social')}}">
+                    <i class="fa fa-facebook-square" aria-hidden="true"></i> <span>Social Networks</span>
                 </a>
             </li>
 
